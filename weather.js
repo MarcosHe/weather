0d19293986973c2980cd8001aac7268693efe90f
MarcosHe <mrnutterbutter@gmail.com>
2017-03-22 23:32:34 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -23,65 +23,67 @@ var response;
     } 
 
     xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            response = JSON.parse(this.responseText);
-        }
+      if(this.readyState == 4 && this.status == 200) {
+        response = JSON.parse(this.responseText);
+        writeWeather(response);    
+      }
     };
 
     function getWeather() {
-        xhttp.open("GET", requestString, true);
-        xhttp.send();
+      xhttp.open("GET", requestString, true);
+      xhttp.send();
     }
 
-  document.getElementById('rain').textContent = (response.currently.precipProbability * 100).toFixed(0) + '%';
+  function writeWeather(response) {
+    document.getElementById('rain').textContent = (response.currently.precipProbability * 100).toFixed(0) + '%';
 
-  document.getElementById('wind-speed').textContent = (response.currently.windSpeed).toFixed(0);
+    document.getElementById('wind-speed').textContent = (response.currently.windSpeed).toFixed(0);
 
-  var windDirection = null;
-  var windDegree = response.currently.windBearing;
-  console.log(windDegree);
-    if (windDegree <= 45 || windDegree >= 315 ){
-      windDirection = "north";
-    } else if (windDegree >= 45 || windDegree <= 135 ){
-      windDirection = "east";
-    } else if (windDegree >= 135 || windDegree <= 225 ){
-      windDirection = "south";
-    } else if (windDegree >= 225 || windDegree <= 315 ){
-      windDirection = "west";
+    var windDirection = null;
+    var windDegree = response.currently.windBearing;
+    console.log(windDegree);
+      if (windDegree <= 45 || windDegree >= 315 ){
+        windDirection = "north";
+      } else if (windDegree >= 45 || windDegree <= 135 ){
+        windDirection = "east";
+      } else if (windDegree >= 135 || windDegree <= 225 ){
+        windDirection = "south";
+      } else if (windDegree >= 225 || windDegree <= 315 ){
+        windDirection = "west";
+      }
+    document.getElementById('wind-direction').textContent = windDirection;
+
+    document.getElementById('temp').textContent = (response.currently.temperature).toFixed(0);
+
+    var iconSrc = null;
+    var icon = response.currently.icon;
+    if (icon == 'clear-day') {
+      iconSrc = 'climacons/Sun.svg';
+    } else if (icon == 'clear-night') {
+      iconSrc = 'climacons/Moon.svg';
+    } else if (icon == 'rain') {
+      iconSrc = 'climacons/Cloud-Rain.svg';
+    } else if (icon == 'snow') {
+      iconSrc = 'climacons/Cloud-Snow.svg';
+    } else if (icon == 'sleet') {
+      iconSrc = 'climacons/Cloud-Hail.svg';
+    } else if (icon == 'wind') {
+      iconSrc = 'climacons/Wind.svg';
+    } else if (icon == 'fog') {
+      iconSrc = 'climacons/Cloud-Fog.svg';
+    } else if (icon == 'cloudy') {
+      iconSrc = 'climacons/Cloud.svg';
+    } else if (icon == 'partly-cloudy') {
+      iconSrc = 'climacons/Cloud-Sun.svg';
+    } else if (icon == 'partly-cloudy-night') {
+      iconSrc = 'climacons/Cloud-Moon.svg';
+    } else {
+      iconSrc = 'climacons/Shades.svg';
     }
-  document.getElementById('wind-direction').textContent = windDirection;
-
-  document.getElementById('temp').textContent = (response.currently.temperature).toFixed(0);
-
-  var iconSrc = null;
-  var icon = response.currently.icon;
-  if (icon == 'clear-day') {
-    iconSrc = 'climacons/Sun.svg';
-  } else if (icon == 'clear-night') {
-    iconSrc = 'climacons/Moon.svg';
-  } else if (icon == 'rain') {
-    iconSrc = 'climacons/Cloud-Rain.svg';
-  } else if (icon == 'snow') {
-    iconSrc = 'climacons/Cloud-Snow.svg';
-  } else if (icon == 'sleet') {
-    iconSrc = 'climacons/Cloud-Hail.svg';
-  } else if (icon == 'wind') {
-    iconSrc = 'climacons/Wind.svg';
-  } else if (icon == 'fog') {
-    iconSrc = 'climacons/Cloud-Fog.svg';
-  } else if (icon == 'cloudy') {
-    iconSrc = 'climacons/Cloud.svg';
-  } else if (icon == 'partly-cloudy') {
-    iconSrc = 'climacons/Cloud-Sun.svg';
-  } else if (icon == 'partly-cloudy-night') {
-    iconSrc = 'climacons/Cloud-Moon.svg';
-  } else {
-    iconSrc = 'climacons/Shades.svg';
+    document.getElementById('weather-icon').src = iconSrc;
   }
-  document.getElementById('weather-icon').src = iconSrc;
-  
-  
-  
-  
+
+
+
 
 }
